afdbb2564fc5648d2a53548b698e6f39c6e55dde
ajaya <ajayasauden.05@gmail.com>
2024-07-08 11:29:25 +0545

    use of restful api and backend data to database

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 
 const express = require('express')
+const { users } = require('./model/index')
 //const app = require('express')()
 const app = express()
 require("./model/index")
 
 app.set('view engine','ejs')
-
+app.use(express.urlencoded({extended : true}))//ssr
+app.use(express.json())//external like react, vuejs
 
 app.get('/',(req,res)=>{
     const name = '- Ajaya Sauden'
@@ -15,6 +17,31 @@ app.get('/',(req,res)=>{
 app.get('/register',(req,res)=>{
     res.render("auth/register")
 })
+
+app.get("/users",async (req,res)=>{
+      const data = await users.findAll()
+      res.json({
+        data
+      })
+})
+
+app.post("/register", async (req,res)=>{
+    // console.log(req.body)
+    const {username,email,password} = req.body
+     //column name: value
+    await users.create({
+        // email:email,
+        // password:password,
+        // username:username
+        email,
+        password,
+        username    
+    })
+    res.send("Registered successfully")
+
+
+})
+
 app.get("/login",(req,res)=>{
     res.render("auth/login")
 })
@@ -26,5 +53,23 @@ const PORT=3000
 app.listen(PORT,()=>{
     console.log(`project has started at PORT ${PORT}`)
 })
+
+
+
+//rest api
+/*
+/getBlogs- get
+/singleblog/:id-get
+/deleteblog/:id-delete
+/addblog-post
+
+*/
+
+//restful api
+/*
+/blogs- get, post
+/blogs/:id - get , patch/put , delete
+
+*/
 
 
